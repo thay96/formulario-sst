aadb651403aa25b3b643cfaebd6b7eebcb9ff253
Equipos <andaraa1996@gmail.com>
2026-09-24 14:13:17 -0500

    actualizar config ssl y scripts de inicio

--- backend/config.js
+++ backend/config.js
@@ -10,7 +10,10 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+  rejectUnauthorized: false
+}
 });
 
 module.exports = pool.promise();
